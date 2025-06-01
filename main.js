fba684132d1e8634bd528e3c5f184a33ebb78a00
ridgway <9522061192@qq.com>
2025-06-01 21:59:47 +0800

    适配mac导出

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+require('@electron/remote/main').initialize()
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -13,9 +14,15 @@ function createWindow () {
     }
   })
 
+  require('@electron/remote/main').enable(win.webContents)
   win.loadFile('app.html')
   // 开发时可以打开开发者工具
   // win.webContents.openDevTools()
+
+  // 添加窗口关闭事件处理
+  win.on('closed', () => {
+    app.quit()
+  })
 }
 
 app.whenReady().then(() => {
